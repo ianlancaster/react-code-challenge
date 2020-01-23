6ab264b85d2b1906b55c31818bcd6a221d8956d4
Ian Lancaster <ianclancaster@gmail.com>
2020-01-23 11:17:43 -0700

    Add ability to update mineral interest.

--- src/routes/EditTractOwnership/EditTractOwnership.component.jsx
+++ src/routes/EditTractOwnership/EditTractOwnership.component.jsx
@@ -12,97 +12,106 @@ import uuidv4 from 'uuid/v4';
 import IconButton from '../../components/IconButton';
 import Icon from '../../components/Icon';
 
-const EditTractOwnership = ({ value = [], onChange = () => { } }) => {
-  const dataMap = Map(value.map(mi => [
-    mi.id,
-    {
-      ...mi,
-      npris: Map((mi.npris || []).map(npri => [npri.id, npri]))
-    }
-  ]));
+const Row = props => (
+  <BaseRow
+    className="p-2"
+    {...props}
+  />
+);
 
-  const [data, setData] = useState(dataMap);
+const TextCol = props => (
+  <Col>
+    <Form.Control {...props}/>
+  </Col>
+);
 
-  const Row = props => (
-    <BaseRow
-      className="p-2"
-      {...props}
-    />
-  );
+const PercentCol = props => (
+  <Col>
+    <InputGroup>
+      <Form.Control {...props} />
+      <InputGroup.Append>
+        <InputGroup.Text>%</InputGroup.Text>
+      </InputGroup.Append>
+    </InputGroup>
+  </Col>
+);
 
-  const TextCol = props => (
-    <Col>
+const IndentTextCol = props => (
+  <Col>
+    <InputGroup>
+      <InputGroup.Prepend style={{ width: 30, marginTop: 10 }}>
+        <Icon icon="indent" />
+      </InputGroup.Prepend>
       <Form.Control {...props}/>
-    </Col>
-  );
+    </InputGroup>
+  </Col>
+);
 
-  const PercentCol = props => (
-    <Col>
-      <InputGroup>
-        <Form.Control {...props} />
-        <InputGroup.Append>
-          <InputGroup.Text>%</InputGroup.Text>
-        </InputGroup.Append>
-      </InputGroup>
-    </Col>
-  );
+const ButtonCol = props => (
+  <Col xs={1}>
+    <Button variant="light" {...props} />
+  </Col>
+);
 
-  const IndentTextCol = props => (
-    <Col>
-      <InputGroup>
-        <InputGroup.Prepend style={{ width: 30, marginTop: 10 }}>
-          <Icon icon="indent" />
-        </InputGroup.Prepend>
-        <Form.Control {...props}/>
-      </InputGroup>
-    </Col>
-  );
+const TableHeader = () => (
+  <Row>
+    <Col>Owner</Col>
+    <Col>Mineral Interest</Col>
+    <Col>NPRI</Col>
+    <Col>Lease</Col>
+    <Col xs={1} />
+  </Row>
+);
 
-  const ButtonCol = props => (
-    <Col xs={1}>
-      <Button variant="light" {...props} />
-    </Col>
-  );
+const NPRI = ({ id, owner, interest }) => (
+  <Row data-testid={`npri-${id}`}>
+    <IndentTextCol
+      value={owner}
+      placeholder="Owner"
+    />
+    <Col/>
+    <PercentCol
+      placeholder="NPRI"
+      value={interest}
+    />
+    <Col />
+    <ButtonCol>
+      <Icon icon="remove" />
+    </ButtonCol>
+  </Row>
+);
 
-  const NPRI = ({ id, owner, interest }) => (
-    <Row data-testid={`npri-${id}`}>
-      <IndentTextCol
-        value={owner}
-        placeholder="Owner"
-      />
-      <Col/>
-      <PercentCol
-        placeholder="NPRI"
-        value={interest}
-      />
-      <Col />
-      <ButtonCol>
-        <Icon icon="remove" />
-      </ButtonCol>
-    </Row>
-  );
-
-  const handleMineralInterestChange = (id, field) => e => {
-    console.log(e.target.value)
+const MineralInterest = ({ id, owner, interest, lease, npris, setData, data }) => {
+  const handleMineralInterestChange = (id, field) => ({ target: { value }}) => {
+    setData(data.set(id, {
+      ...data.get(id),
+      [field]: value
+    }))
   };
 
-  const MineralInterest = ({ id, owner, interest, lease, npris }) => (
+  return (
     <>
-      <Row data-testid={`mineralInterest-${id}`}>
+      <Row
+        data-testid={`mineralInterest-${id}`}
+        key={`mineralInterest-${id}`}
+      >
         <TextCol
           placeholder="Owner"
           onChange={handleMineralInterestChange(id, 'owner')}
           data-testid={`mineralInterest-${id}.owner`}
+          key={`mineralInterest-${id}.owner`}
           value={owner}
         />
         <PercentCol
           placeholder="Mineral Interest"
+          onChange={handleMineralInterestChange(id, 'interest')}
           data-testid={`mineralInterest-${id}.interest`}
           value={interest}
         />
         <Col/>
         <TextCol
           placeholder="Lease"
+          onChange={handleMineralInterestChange(id, 'lease')}
           data-testid={`mineralInterest-${id}.lease`}
           value={lease}
         />
@@ -118,16 +127,27 @@ const EditTractOwnership = ({ value = [], onChange = () => { } }) => {
       </Row>
     </>
   );
+};
 
-  const TableHeader = () => (
-    <Row>
-      <Col>Owner</Col>
-      <Col>Mineral Interest</Col>
-      <Col>NPRI</Col>
-      <Col>Lease</Col>
-      <Col xs={1} />
-    </Row>
-  );
+const EditTractOwnership = ({ value = [], onChange = () => { } }) => {
+  const dataMap = Map(value.map(mi => [
+    mi.id,
+    {
+      ...mi,
+      npris: Map((mi.npris || []).map(npri => [npri.id, npri]))
+    }
+  ]));
+
+  const [data, setData] = useState(dataMap);
+
+  useEffect(() => {
+    const normalizedData = data.toArray().map(mi => ({
+      ...mi[1],
+      npris: mi[1].npris.toArray().map(npri => npri[1])
+    }));
+    // debugger
+    onChange(normalizedData)
+  });
 
   const createEmptyMineralInterest = () => ({
     id: uuidv4(),
@@ -142,19 +162,17 @@ const EditTractOwnership = ({ value = [], onChange = () => { } }) => {
     setData(data.set(mi.id, mi));
   };
 
-  useEffect(() => {
-    onChange(data.toArray().map(mi => ({
-      ...mi[1],
-      npris: mi[1].npris.toArray().map(npri => npri[1])
-    })))
-  });
-
   return (
     <Form>
       <Container>
         <TableHeader />
         {data.entrySeq().map(([key, value]) => (
-          <MineralInterest key={key} {...value} />
+          <MineralInterest
+            key={key}
+            data={data}
+            setData={setData}
+            {...value}
+          />
         ))}
         <Row>
           <Col>
